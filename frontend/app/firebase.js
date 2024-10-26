022d611ae3145fa04c54d6ecc4f4cca3173c3628
Samuel Michnik <michniksamuel@gmail.com>
2024-10-26 18:28:55 -0400

    secured api keys

--- frontend/app/firebase.js
+++ frontend/app/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyCrQwib3Cv2EtnXTj04mAThast6Qb0EarY",
-  authDomain: "alignly-98902.firebaseapp.com",
-  projectId: "alignly-98902",
-  storageBucket: "alignly-98902.appspot.com",
-  messagingSenderId: "335403802321",
-  appId: "1:335403802321:web:e9cb1c60caae3f37762ff0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
